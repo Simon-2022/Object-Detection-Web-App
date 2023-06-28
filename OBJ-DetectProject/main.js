a600832264e7cd0ce3f10a9d1c409ae8350fc3ee
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-06-28 12:41:50 +1000

    Add files via upload

--- OBJ-DetectProject/main.js
+++ OBJ-DetectProject/main.js
@@ -39,3 +39,65 @@ function draw() {
   // Place the webcam live on the canvas
   image(video, 0, 0, width, height);
 }
+
+// Define gotResult() function
+function gotResult(error, results) {
+    // Fetch results from the model
+    if (error) {
+      console.error(error);
+    } else {
+      // Assign results array to the defined array
+      resultsArray = results;
+    }
+  }
+  
+  // Define an empty array to store results
+  let resultsArray = [];
+  
+  // p5.js draw() function
+  function draw() {
+    // Check if the model status is not empty
+    if (status) {
+      // Loop through the objects array
+      for (let i = 0; i < resultsArray.length; i++) {
+        const object = resultsArray[i];
+        // Fetch confidence and convert it to percentage
+        const confidence = Math.round(object.confidence * 100);
+  
+        // Fetch label
+        const label = object.label;
+  
+        // Fetch x and y coordinates
+        const x = object.x;
+        const y = object.y;
+  
+        // Place label and confidence near the object
+        text(label + ": " + confidence + "%", x + 10, y + 20);
+  
+        // Fetch width, height, x, and y coordinates and draw a rectangle
+        const width = object.width;
+        const height = object.height;
+        rect(x, y, width, height);
+  
+        // Check if the label matches the object mentioned
+        if (label === objectMentioned) {
+          // Stop the webcam live view
+          video.stop();
+  
+          // Stop the execution of the cocossd model
+          cocossd.dispose();
+  
+          // Update the HTML element with the status of the object found
+          document.getElementById('status').innerText = 'Object mentioned found';
+  
+          // Use SpeechSynthesisUtterance for text-to-speech
+          const utterThis = new SpeechSynthesisUtterance('Object mentioned found');
+          speechSynthesis.speak(utterThis);
+        }
+      }
+    } else {
+      // Update the HTML element with the status of the object not found
+      document.getElementById('status').innerText = 'Object mentioned not found';
+    }
+  }
+  
